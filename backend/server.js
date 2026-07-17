7021d1c49bd4037ecb4b9663711b310fa8a92ff7
SOHAM160 <154835061+SOHAM160@users.noreply.github.com>
2026-07-18 00:28:36 +0530

    fix: add x-timezone to CORS allowedHeaders

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-timezone'],
 }));
 
 // Body parsing
